b7afd58d4a9091488025dfda4d726cf7e29f0a17
daeyeol-yang <97139430+daeyeol-yang@users.noreply.github.com>
2025-05-16 17:56:33 +0900

    feat : ai vue 기능추가

--- src/main/java/com/ssafy/home/starred/controller/StarredController.java
+++ src/main/java/com/ssafy/home/starred/controller/StarredController.java
@@ -1,10 +1,14 @@
 package com.ssafy.home.starred.controller;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +18,7 @@ import com.ssafy.home.common.RestControllerHelper;
 import com.ssafy.home.member.model.dto.MemberDTO;
 import com.ssafy.home.security.dto.CustomUserDetails;
 import com.ssafy.home.starred.model.dto.StarredDTO;
+import com.ssafy.home.starred.model.dto.StarredHouseInfoDTO;
 import com.ssafy.home.starred.model.service.StarredService;
 
 import jakarta.servlet.http.HttpSession;
@@ -40,6 +45,23 @@ public class StarredController implements RestControllerHelper {
 			e.printStackTrace();
 			return handleFail(e); 
 		}
+		
+	}
+	
+	@GetMapping
+	public ResponseEntity<?> starredList(@AuthenticationPrincipal CustomUserDetails member){
+		try {
+			System.out.println("관심지역리스트 도착");
+			int memberId = member.getMember().getId();
+			StarredDTO starred = StarredDTO.builder().userId(memberId).build();
+			List<StarredHouseInfoDTO> starredList = sService.findStarred(starred);
+			System.out.println(starredList);
+			return handleSuccess(Map.of("data", starredList));
+		}catch(Exception e) {
+			e.printStackTrace();
+			return handleFail(e);
+		}
+		
 		
 	}
 	
